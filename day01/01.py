b826a4326b91e5c18e3056841edacc925d0e9c3b
jablka <mrnkpst@gmail.com>
2022-12-03 17:59:42 +0100

    refactoring
    little refactoring

--- day01/01.py
+++ day01/01.py
@@ -1,11 +1,29 @@
-with open('input.txt', encoding="utf-8") as f:
-    data_string = f.read()
 
-data_lines = data_string.splitlines()
+# following is a test string in case you need it:
+data='''1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000'''
+
+# or get the data straight from the file:
+with open('input.txt', encoding="utf-8") as f:
+    data = f.read()
+
+data = data.splitlines()
 
 elfs = list()
 elf_particular = list()
-for line in data_lines:
+for line in data:
     if line != '':
         elf_particular.append(int(line))
     else:
