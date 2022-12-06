11eacaef0018f58fd8c5ce1ff3580ac6cdc4ce7b
jablka <mrnkpst@gmail.com>
2022-12-06 11:15:36 +0100

    Create part2_elegant.py

---
+++ day05/part2_elegant.py
@@ -0,0 +1,79 @@
+# based on part1_elegant.py
+
+import re
+
+with open('input.txt', encoding="utf-8") as f:
+    data = f.read()
+
+# rozdelíme na data[0] a data[1]
+data = data.split('\n\n')
+
+# first part of the data
+s_lines = data[0].splitlines()
+
+#     [D]    
+# [N] [C]    
+# [Z] [M] [P]
+#  1   2   3 
+
+# 'number_of_stacks' is the second character from the end, in the last line.
+number_of_stacks = int(s_lines[-1][-2])
+
+# 'stacks' will be a list of lists
+stacks = [ ]
+for i in range(number_of_stacks):
+    stacks.append([ ])
+
+# let's fill our stacks
+for riadok in s_lines:
+    for i,pismeno in enumerate(riadok[1::4]):
+        if pismeno.isalpha():
+            stacks[i].append(pismeno)
+
+# =======
+# moves
+# =======
+
+# move 1 from 2 to 1
+# move 3 from 1 to 3
+# move 2 from 2 to 1
+# move 1 from 1 to 2
+
+# data[1] # second part of the data... contains 'moves data'
+moves = data[1].strip().splitlines() # list of moves 
+
+# moves_mustra - will contain just the numbers, something like: [3,2,1]
+moves_mustra = [ ]
+for e in moves:
+    najdene = re.findall('\d+', e)
+    pomocny = list(map(int, najdene)) # pomocny = [howmany, from, to]
+    moves_mustra.append(pomocny)
+
+# ======================================
+# máme mustry: 'stacks' & 'mustra_moves'
+# teraz už len vykonať pohyby
+# ======================================
+
+# stacks need to be reversed, because we have read them from top-to-bottom
+for stack in stacks:
+    stack.reverse()
+
+for line in moves_mustra:
+
+    _helper = [ ]
+    howmany = line[0] # line[0] stands for 'how many'
+    for h in range(howmany):
+        tt = stacks[line[1]-1].pop()
+        _helper.append(tt)
+
+    for _h in range(len(_helper)):
+        ttt = _helper.pop()
+        stacks[line[2]-1].append(ttt)
+
+vysledok = ''
+for e in stacks:
+    vysledok += e.pop()
+    # print(e.pop())
+
+print(vysledok)
+# HOTOVO!
